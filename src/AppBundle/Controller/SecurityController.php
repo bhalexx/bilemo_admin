7801691f06a80884ff8144a5bdeb1462669e4a7a
Bhalexx <minia-ture@hotmail.com>
2018-05-01 10:07:45 +0200

    PSR & Symfony best practices

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -1,28 +1,28 @@
 <?php
 
-	namespace AppBundle\Controller;
+namespace AppBundle\Controller;
 
-	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-	use Symfony\Component\HttpFoundation\Request;
-	use Symfony\Component\Routing\Annotation\Route;
-	use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-	class SecurityController extends Controller
+class SecurityController extends Controller
+{
+	/**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request, AuthenticationUtils $authUtils)
 	{
-		/**
-	     * @Route("/login", name="login")
-	     */
-	    public function loginAction(Request $request, AuthenticationUtils $authUtils)
-		{
-		    // get the login error if there is one
-		    $error = $authUtils->getLastAuthenticationError();
+	    // get the login error if there is one
+	    $error = $authUtils->getLastAuthenticationError();
 
-		    // last username entered by the user
-		    $lastUsername = $authUtils->getLastUsername();
+	    // last username entered by the user
+	    $lastUsername = $authUtils->getLastUsername();
 
-		    return $this->render('security/login.html.twig', [
-		        'last_username' => $lastUsername,
-		        'error'         => $error,
-		    ]);
-		}
+	    return $this->render('security/login.html.twig', [
+	        'last_username' => $lastUsername,
+	        'error'         => $error,
+	    ]);
 	}
+}
